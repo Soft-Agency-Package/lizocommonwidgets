caa96171cbdc64f5e372f16aaeccf204f332724c
mrbokaagny <mrbokableagny@gmail.com>
2025-08-28 08:45:27 +0000

    update oath_layout

--- lib/components/layouts/oauth_layout.dart
+++ lib/components/layouts/oauth_layout.dart
@@ -5,25 +5,33 @@ class OauthLayout extends StatelessWidget {
   final double width;
   final Widget child;
   final double height;
+  final Function onTap;
+  final Color bgColor;
 
   const OauthLayout({
     super.key,
     required this.width,
     required this.child,
     required this.height,
+    required this.onTap,
+    this.bgColor = Colors.white,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: width,
-      height: height,
-      padding: EdgeInsets.all(8.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(24.0),
-        border: Border.all(color: LizoColor.otherBorder, width: 2.37),
+    return InkWell(
+      onTap: () => onTap(),
+      child: Container(
+        width: width,
+        height: height,
+        padding: EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          color: bgColor,
+          borderRadius: BorderRadius.circular(24.0),
+          border: Border.all(color: LizoColor.otherBorder, width: 2.37),
+        ),
+        child: child,
       ),
-      child: child,
     );
   }
 }
